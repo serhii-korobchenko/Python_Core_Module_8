fd44ea72b41f2d12550ed36a2c1cf769f1141c1e
Serhii Korobchenko <jchild2008@gmail.com>
2022-07-25 19:58:41 +0300

    Update module 8

---
+++ Lectures/package_datetime.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+
+# current_datetime
+current_datetime = datetime.now()
+print(current_datetime) # 
+
+# datatime with atributes
+
+print(current_datetime.year)        # 2020
+print(current_datetime.month)       # 10
+print(current_datetime.day)         # 09
+print(current_datetime.hour)        # 22
+print(current_datetime.minute)      # 32
+print(current_datetime.second)      # 22
+print(current_datetime.microsecond) # 819366
+
+# datatime methods
+print(current_datetime.date())  # 2020-10-09
+print(current_datetime.time())  # 22:13:35.053819
+# set up data 
+d1 = datetime(year=2012, month=1, day=7, hour=14)
+print(d1) # 2012-01-07 14:00:00
